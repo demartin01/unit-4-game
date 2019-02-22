e1997e12261d734cc3f1344208077e559f434f10
denise martin <denise.martin@vanderbilt.edu>
2019-02-21 23:34:47 -0600

    changes made to games.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
-//$(document).ready(function(){} --why does this break my code?
+// $(document).ready(function(){} --why does this break my code?
     var wins = 0;
     var losses = 0;
     var random = 0;
     var images = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
-
+    var score = 0; //get value of totalScore element
 
     renderImages();
 
@@ -20,14 +20,24 @@
         for (let i = 0; i < images.length; i++) {
 
             var crystal = $("<img>");
+            crystal.attr("id", "image-" + (i + 1))
             crystal.attr("src", images[i]);
             crystal.addClass("crystals");
             crystal.attr("data-id", Math.floor(Math.random() * 11 + 1));
 
-            $("#imgDiv").append(crystal);
+          
+            crystal.click(function(event){
+            // need to dump this content into totalsum
+            //$( "div" ).data( "role" );
+            ($(this).data( "id" ));
+            $(this).data( "id" )
+            });
+
+            $("#imgDiv").append(crystal);  
         }
     }
    /*Create listening event $("images").click(function(){} or on click? What's the difference 
+
    get value & add to total score 
    
    if (matchThis ===total sum){
