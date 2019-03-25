ccfd97dd7e1152d478425f09ce03292672f87082
denise martin <denise.martin@vanderbilt.edu>
2019-03-25 17:40:46 -0500

    changes to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,20 @@
-// $(document).ready(function(){} --why does this break my code?
+
+// $(document).ready(function () {
+
     var wins = 0;
     var losses = 0;
     var random = 0;
     var images = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/crystal4.jpg"];
     var score = 0; //get value of totalScore element
-
-    renderImages();
+    // var total = 0
 
     startGame();
 
     function startGame() {
         random = Math.floor(Math.random() * 101 + 19);
         $("#matchThis").text(random);
+        total = 0;
+        $("#totalSum").text(total);
         renderImages();
     }
 
@@ -25,36 +28,60 @@
             crystal.addClass("crystals");
             crystal.attr("data-id", Math.floor(Math.random() * 11 + 1));
 
-          
-            crystal.click(function(event){
-            // need to dump this content into totalsum
-            //$( "div" ).data( "role" );
-            ($(this).data( "id" ));
-            $(this).data( "id" )
+
+            crystal.click(function (event) {
+                // need to dump this content into totalsum
+                //$( "div" ).data( "role" );
+                ($(this).data("id"));
+                $(this).data("id")
             });
 
-            $("#imgDiv").append(crystal);  
+            $("#imgDiv").append(crystal);
         }
     }
-   /*Create listening event $("images").click(function(){} or on click? What's the difference 
+    //Create listening event $(".rystals").click(function(){} or on click? What's the difference 
+    $(document).on("click", ".crystals", function () {
+        
+    //get value & add to total score 
+        total += parseInt($(this).attr("data-id"));
+        $("#totalSum").text(total);
 
-   get value & add to total score 
-   
-   if (matchThis ===total sum){
-    Alert ("Yay, You Win!");
-   } 
-                -->then add one to wins
-    
-    else if (matchThis >=total sum){  
-    Alert ("Sorry, You Lose!");
-    }
-                -->then add one to loses 
+        if (total === random ) {
+            alert("Yay, you Win!");
+            wins++;
+            $("#totalWins").text(wins);
+            startGame();
+        } 
+        else if (total > random) {
+            alert("Sorry, You lose!");
+            losses++;
+            $("#losses").text(losses);
+            startGame();
+        }
 
-    keep count/Tally of loses and wins --Counter or New Score? 
-    */
-
-
-   
+    })
 
 
+
+
+
+    //    if (matchThis===total sum){
+    //     Alert ("Yay, You Win!");
+    //    } 
+    //                 -->then add one to wins
+
+    //     else if (matchThis >=total sum){  
+    //     Alert ("Sorry, You Lose!");
+    //     }
+    //                 -->then add one to loses 
+
+    //     keep count/Tally of loses and wins --Counter or New Score? 
+
+
+
+
+
+
+// }); 
+// --why does this break my code ?
 
